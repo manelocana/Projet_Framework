45cd9e1e18a2f9e0569105b8698ef0c0c56eead9
manelocana <manelocana.dev@gmail.com>
2025-12-02 00:45:53 +0100

    continue adjust links home.html, add emojis lol, corrections

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -8,16 +8,7 @@ def pages():
     return render_template('pages.html', search_button=True)
 
 
-@pages_bp.route('/pages/projet_1')
-def projet_1():
-    return render_template('projet_1.html', search_button=True)
-
-
 @pages_bp.route('/pages/branding')
 def branding():
     return render_template('projet_branding.html', search_button=True)
 
-
-@pages_bp.route('/pages/projet1_photos')
-def projet1_photos():
-    return render_template('projet1_photos.html', search_button=True)
--- app/routes/portfolio.py
+++ app/routes/portfolio.py
@@ -6,3 +6,13 @@ portfolio_bp = Blueprint('portfolio', __name__)
 @portfolio_bp.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html', search_button=True)
+
+
+@portfolio_bp.route('/portfolio/projet_1')
+def projet_1():
+    return render_template('portfolio/projet_1.html', search_button=True)
+
+
+@portfolio_bp.route('/portfolio/projet1_photos')
+def projet1_photos():
+    return render_template('portfolio/projet1_photos.html', search_button=True)
