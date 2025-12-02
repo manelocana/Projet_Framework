2bcda71c3fef81e8763a9fa8f4b8b62d3b46e56d
manelocana <manelocana.dev@gmail.com>
2025-12-02 02:01:05 +0100

    delete __pycache__ and update gitignore

--- app/routes/blog.py
+++ app/routes/blog.py
@@ -6,3 +6,8 @@ blog_bp = Blueprint('blog', __name__)
 @blog_bp.route('/blog')
 def blog():
     return render_template('blog.html', search_button=True)
+
+
+@blog_bp.route('/blog/article')
+def blog_article():
+    return render_template('landing_blog.html', search_button=True)
