16f6192208cc49fb795ad2a6dfb93543435a1bb7
manelocana <manelocana.dev@gmail.com>
2026-01-16 07:15:10 +0100

    add logo , and more clean-refactor-css ...  now is better ...

--- app/routes/home.py
+++ app/routes/home.py
@@ -5,7 +5,22 @@ from app.models.portfolio import Project
 home_bp = Blueprint('home', __name__)
 
 
-@home_bp.route('/')
-def home():
+
+def get_projects():
     projects = Project.query.order_by(Project.id.desc()).all()
     return render_template('home.html', projects=projects, title='Home')
+
+def get_posts():
+    posts = [
+        {"title": "Primer Post", "image": "home_cards_1.jpeg", "url": "/post/1"},
+        {"title": "Segundo Post", "image": "home_cards_2.jpeg", "url": "/post/2"},
+        {"title": "Tercer Post", "image": "home_cards_3.jpeg", "url": "/post/3"},
+    ]
+    return render_template('home.html', posts=posts)
+
+
+@home_bp.route('/')
+def home():
+    projects = get_projects()
+    posts = get_posts()
+    return render_template('home.html', projects=projects, posts=posts, title='Home')
