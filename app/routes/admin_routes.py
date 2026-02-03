d4efc4c9efb91d4dd0d432edae783da6a156ddac
manelocana <manelocana.dev@gmail.com>
2026-02-03 15:30:56 +0100

    i start work in dashboard, route, css, i see users ok

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -5,11 +5,12 @@ from flask import Blueprint, render_template
 from flask_login import login_required
 from app.decorators import role_required
 
+from app.models.user import User
+
 
 
 
 admin_bp = Blueprint("admin", __name__)
-
 
 
 
@@ -20,3 +21,13 @@ admin_bp = Blueprint("admin", __name__)
 @role_required(["admin"])
 def admin_dashboard():
     return render_template("admin/dashboard.html")
+
+
+
+
+@admin_bp.route('/admin/users')
+@login_required
+@role_required(['admin'])
+def admin_users():
+    users = User.query.order_by(User.id.asc()).all()
+    return render_template('admin/users.html', users=users)
