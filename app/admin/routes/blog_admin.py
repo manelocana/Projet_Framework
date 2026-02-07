5b56178984c3f712940037f4617ffac66f8b3e9d
manelocana <manelocana.dev@gmail.com>
2026-02-07 19:07:11 +0100

    separate logic for save/delete image from post-blog , utils/image_utils.py... for more easy read methods blog_admin

--- app/admin/routes/blog_admin.py
+++ app/admin/routes/blog_admin.py
@@ -1,13 +1,13 @@
 
 
 
-from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask import Blueprint, render_template, request, redirect, url_for
 from app.models.post import Post
 from app.extensions import db
-import os
-from werkzeug.utils import secure_filename
 from flask_login import login_required
 from app.forms.blog import BlogForm
+
+from utils.image_utils import save_blog_image, delete_blog_image
 
 
 
@@ -21,18 +21,15 @@ def blog_new():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form.get('content')
-        image_file = request.files.get('image')
 
-        image_filename=None
-        if image_file and image_file.filename:
-            image_filename = secure_filename(image_file.filename)
-            upload_path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'blog')
-            os.makedirs(upload_path, exist_ok=True)
-            image_file.save(os.path.join(upload_path, image_filename))
+        image_file = request.files.get('image')
+        image_filename = save_blog_image(image_file)
         
         new_post = Post(title=title, content=content, image=image_filename)
+
         db.session.add(new_post)
         db.session.commit()
+
         return redirect(url_for('blog.blog'))
     return render_template('admin/blog/blog_new.html')
 
@@ -52,20 +49,11 @@ def blog_edit(post_id):
         # voir si on monte le img
         image_file = request.files.get('image')
         if image_file and image_file.filename:
-            image_filename = secure_filename(image_file.filename)
-            upload_path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'blog')
-            os.makedirs(upload_path, exist_ok=True)
-
-            # effacer img anncienne
-            if post.image:
-                old_path = os.path.join(upload_path, post.image)
-                if os.path.exists(old_path):
-                    os.remove(old_path)
-
-            image_file.save(os.path.join(upload_path, image_filename))
-            post.image = image_filename
+            delete_blog_image(post.image)
+            post.image = save_blog_image(image_file)
 
         db.session.commit()
+
         return redirect(url_for('blog.blog_article', post_id=post.id))
     return render_template('admin/blog/blog_edit.html', post=post, form=form)
 
@@ -77,11 +65,7 @@ def blog_edit(post_id):
 def blog_delete(post_id):
     post = Post.query.get_or_404(post_id)
 
-    if post.image:
-        image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'blog', post.image)
-        
-        if os.path.exists(image_path):
-            os.remove(image_path)
+    delete_blog_image(post.image)
 
     db.session.delete(post)
     db.session.commit()
---
+++ utils/image_utils.py
@@ -0,0 +1,41 @@
+
+
+
+import os
+import uuid
+from werkzeug.utils import secure_filename
+from flask import current_app
+
+
+
+
+def save_blog_image(image_file):
+    if not image_file or not image_file.filename:
+        return None
+
+    original_name = secure_filename(image_file.filename)
+    unique_name = f"{uuid.uuid4().hex}_{original_name}"
+
+    upload_path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'blog')
+    os.makedirs(upload_path, exist_ok=True)
+
+    image_file.save(os.path.join(upload_path, unique_name))
+
+    return unique_name
+
+
+
+
+
+def delete_blog_image(filename):
+    if not filename:
+        return
+
+    path = os.path.join(
+        current_app.config['UPLOAD_FOLDER'],
+        'blog',
+        filename
+    )
+
+    if os.path.exists(path):
+        os.remove(path)
